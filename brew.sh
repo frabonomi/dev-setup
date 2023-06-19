69d0430d696216007f4182b402f1411a0ded225d
Francesco Bonomi <frbonomi@gmail.com>
2023-06-20 01:34:24 +0200

    chore: add cli tools installation via Homebrew

---
+++ brew.sh
@@ -0,0 +1,24 @@
+tap "homebrew/bundle"
+tap "homebrew/core"
+tap "lotyp/homebrew-formulae"
+
+# core/dotfiles
+brew "lotyp/formulae/dockutil"
+
+# fs/network
+brew "bat"
+brew "httpie"
+brew "ngrok"
+brew "wget"
+
+# search/grep/diff
+brew "diff-so-fancy"
+brew "difftastic"
+brew "fd"
+brew "grep"
+brew "psgrep"
+
+# languages/tools
+brew "gh"
+brew "nvm"
+brew "python"
